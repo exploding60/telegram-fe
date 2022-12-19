6a567ed3db3e9b33ebff574f4fdd0da304293ea4
exploding60 <rizkysyahputra14045@gmail.com>
2022-12-19 10:35:45 +0700

    Fix Redux

--- src/pages/redux/actions/login.js
+++ src/pages/redux/actions/login.js
@@ -1,15 +1,17 @@
 import axios from "axios";
 import { Next } from "react-bootstrap/esm/PageItem";
 import { Navigate } from "react-router-dom";
-export const loginUser = (data, navigate) => async (dispact) => {
+
+export const loginUser = (data, navigate) => async (dispacth) => {
   try {
     console.log(data);
-    dispact({ type: "USER_LOGIN_PENDING" });
-    const result = await axios.get(`http://localhost:3009/auth/login`, data);
+    dispacth({ type: "USER_LOGIN_PENDING" });
+    const result = await axios.post(`http://localhost:3009/auth/login`, data);
     const user = result.data.data;
     console.log(user, "user");
     localStorage.setItem("token", user.token);
-    dispact({ type: "USER_LOGIN_SUCCESS", payload: user });
+    localStorage.setItem("username", user.username);
+    dispacth({ type: "USER_LOGIN_SUCCESS", payload: user });
     alert("Berhasil Login");
     navigate("/");
   } catch (err) {
